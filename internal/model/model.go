701d177915cb5dcc3a97d4788938276b1ef657de
AlexeyBorisovets <borisan4@gmail.com>
2022-12-08 17:03:28 +0300

    delete unnecessary code

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,7 +1,6 @@
 // Package model models that use in this project
 package model
 
-
 type User struct {
 	ID           string `bson,json:"id"`
 	Name         string `bson,json:"name"`
@@ -9,14 +8,9 @@ type User struct {
 	Balance      uint   `bson,json:"balance"`
 	UserType     string `bson,json:"usertype"`
 	RefreshToken string `bson,json:"refreshtoken"`
-	
 }
-
 
 // Config struct create config
 type Config struct {
-	CurrentDB     string `env:"CURRENT_DB" envDefault:"postgres"`
-	PostgresDBURL string `env:"POSTGRES_DB_URL"`
-	MongoDBURL    string `env:"MONGO_DB_URL"`
-	JwtKey        []byte `env:"JWT-KEY" `
+	JwtKey []byte `env:"JWT-KEY" `
 }
